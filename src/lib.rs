8dc4894e5248509a324d634ec4756b218dab1040
jawoznia <jnwznk@gmail.com>
2022-07-10 19:06:40 +0200

    Going deeper into contract.

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,19 @@ use cosmwasm_std::{
     entry_point, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
 };
 
+mod contract;
+mod msg;
+
 #[entry_point]
-pub fn instantiate(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: Empty,
-) -> StdResult<Response> {
-    Ok(Response::new())
+pub fn instantiate(deps: DepsMut, env: Env, info: MessageInfo, msg: Empty)
+  -> StdResult<Response>
+{
+    contract::instantiate(deps, env, info, msg)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
-    }
+#[entry_point]
+pub fn query(deps: Deps, env: Env, msg: msg::QueryMsg)
+  -> StdResult<Binary>
+{
+    contract::query(deps, env, msg)
 }
