cc23e0b30d593f3e99481625be90ee43e4b8ad7c
jawoznia <jnwznk@gmail.com>
2022-08-04 13:34:55 +0200

    Added missing messages.

--- contract-vote/src/msg.rs
+++ contract-vote/src/msg.rs
@@ -5,20 +5,17 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct InstantiateMsg {
-    pub addr: String,
     pub required: u32,
+    pub voting_admins: Vec<Addr>,
 }
 
 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-
-    
+    AddVote { approved: bool },
 }
 
 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    Greet {},
-    AdminsList {},
 }
