fe689860d674b181d35d0f234b9e08204216e28d
jawoznia <jnwznk@gmail.com>
2022-08-04 18:51:33 +0200

    Added missing Result types to lib.rs

--- contract-vote/src/lib.rs
+++ contract-vote/src/lib.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{entry_point, Deps, DepsMut, Env, MessageInfo, StdResult};
+use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 use msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 pub mod contract;
@@ -11,17 +11,17 @@ pub fn instantiate(
     env: Env,
     info: MessageInfo,
     msg: InstantiateMsg,
-) -> StdResult<()> {
+) -> StdResult<Response> {
     contract::instantiate(deps, env, info, msg)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> StdResult<()> {
+pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> StdResult<Response> {
     use contract::execute;
     execute(deps, env, info, msg)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<()> {
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
     contract::query(deps, env, msg)
 }
