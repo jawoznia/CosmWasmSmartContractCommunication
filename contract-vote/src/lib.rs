3ce45cd82d7b69fc1f8bab4bc0aa12e711af7131
jawoznia <jnwznk@gmail.com>
2022-08-05 10:34:32 +0200

    Added vote to admin Cargo.toml

--- contract-vote/src/lib.rs
+++ contract-vote/src/lib.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 use msg::{AcceptMsg, InstantiateMsg, QueryMsg};
 
 pub mod contract;
