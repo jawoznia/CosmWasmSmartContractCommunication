06edf7a64c0478f64c9aa19dc062720fd20d36b0
jawoznia <jnwznk@gmail.com>
2022-08-10 14:08:13 +0200

    Duplicated votes covered.

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,8 +1,7 @@
 #[cfg(test)]
 mod tests {
 
-    
-    use cosmwasm_std::{coins, from_binary, Addr, Empty};
+    use cosmwasm_std::{coins, from_binary, Addr, BlockInfo, Empty};
     use cw_multi_test::{App, AppResponse, Contract, ContractWrapper, Executor};
     use msgs::admin::{
         AdminsListResp, ExecuteMsg as AdminExecuteMsg, InstantiateMsg as AdminInstantiateMsg,
@@ -289,5 +288,243 @@ mod tests {
             .unwrap();
 
         assert_eq!(resp.admins.len(), 3);
+    }
+
+    #[test]
+    fn duplicated_votes() {
+        let mut app = App::new(|router, _api, storage| {
+            router
+                .bank
+                .init_balance(storage, &Addr::unchecked("admin1"), coins(100, "utgd"))
+                .unwrap();
+        });
+        let admin_code_id = app.store_code(admin());
+        let vote_code_id = app.store_code(vote());
+
+        let admin = app
+            .instantiate_contract(
+                admin_code_id,
+                Addr::unchecked("owner"),
+                &AdminInstantiateMsg {
+                    admins: vec![
+                        "owner".to_owned(),
+                        "admin1".to_owned(),
+                        "admin2".to_owned(),
+                        "admin3".to_owned(),
+                    ],
+                    donation_denom: "eth".to_owned(),
+                    vote_code_id: vote_code_id,
+                },
+                &[],
+                "vote",
+                None,
+            )
+            .unwrap();
+
+        let resp: AppResponse = app
+            .execute_contract(
+                Addr::unchecked("owner"),
+                admin.clone(),
+                &AdminExecuteMsg::ProposeAdmin {
+                    addr: String::from("new_admin"),
+                    required_votes: 3,
+                    admin_code_id,
+                },
+                &[],
+            )
+            .unwrap();
+
+        let vote: Addr = from_binary(&resp.data.unwrap()).unwrap();
+
+        app.execute_contract(
+            Addr::unchecked("admin1"),
+            vote.clone(),
+            &VoteExecuteMsg::Accept {},
+            &[],
+        )
+        .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(admin.clone(), &AdminQueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp.admins.len(), 4);
+
+        app.execute_contract(
+            Addr::unchecked("admin1"),
+            vote.clone(),
+            &VoteExecuteMsg::Accept {},
+            &[],
+        )
+        .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(admin.clone(), &AdminQueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp.admins.len(), 4);
+
+        app.execute_contract(
+            Addr::unchecked("admin2"),
+            vote.clone(),
+            &VoteExecuteMsg::Accept {},
+            &[],
+        )
+        .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(admin.clone(), &AdminQueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp.admins.len(), 4);
+
+        app.execute_contract(
+            Addr::unchecked("admin1"),
+            vote.clone(),
+            &VoteExecuteMsg::Accept {},
+            &[],
+        )
+        .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(admin.clone(), &AdminQueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp.admins.len(), 4);
+
+        app.execute_contract(
+            Addr::unchecked("admin3"),
+            vote.clone(),
+            &VoteExecuteMsg::Accept {},
+            &[],
+        )
+        .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(admin.clone(), &AdminQueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp.admins.len(), 5);
+    }
+
+    #[test]
+    // Ignored because
+    #[ignore]
+    fn accepting_vote_older_than_admin() {
+        let mut app = App::new(|router, _api, storage| {
+            router
+                .bank
+                .init_balance(storage, &Addr::unchecked("admin1"), coins(100, "utgd"))
+                .unwrap();
+        });
+        let admin_code_id = app.store_code(admin());
+        let vote_code_id = app.store_code(vote());
+
+        let admin = app // contract0
+            .instantiate_contract(
+                admin_code_id,
+                Addr::unchecked("owner"),
+                &AdminInstantiateMsg {
+                    admins: vec![String::from("owner"), String::from("admin1")],
+                    donation_denom: "eth".to_owned(),
+                    vote_code_id: vote_code_id,
+                },
+                &[],
+                "vote",
+                None,
+            )
+            .unwrap();
+
+        let resp: AppResponse = app
+            .execute_contract(
+                Addr::unchecked("owner"),
+                admin.clone(),
+                &AdminExecuteMsg::ProposeAdmin {
+                    addr: String::from("admin2"),
+                    required_votes: 1,
+                    admin_code_id,
+                },
+                &[],
+            )
+            .unwrap();
+
+        let vote_admin_2: Addr = from_binary(&resp.data.unwrap()).unwrap();
+
+        let resp: AppResponse = app
+            .execute_contract(
+                Addr::unchecked("owner"),
+                admin.clone(),
+                &AdminExecuteMsg::ProposeAdmin {
+                    addr: String::from("admin3"),
+                    required_votes: 1,
+                    admin_code_id,
+                },
+                &[],
+            )
+            .unwrap();
+
+        let vote_admin_3: Addr = from_binary(&resp.data.unwrap()).unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(admin.clone(), &AdminQueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp.admins.len(), 2);
+
+        app.update_block(|bi: &mut BlockInfo| {
+            bi.time.plus_seconds(5);
+            bi.height.checked_add(5).unwrap();
+        });
+
+        app.execute_contract(
+            Addr::unchecked("admin1"),
+            vote_admin_2,
+            &VoteExecuteMsg::Accept {},
+            &[],
+        )
+        .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(admin.clone(), &AdminQueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp.admins.len(), 3);
+
+        app.execute_contract(
+            Addr::unchecked("admin2"),
+            vote_admin_3.clone(),
+            &VoteExecuteMsg::Accept {},
+            &[],
+        )
+        .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(admin.clone(), &AdminQueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp.admins.len(), 3);
+
+        app.execute_contract(
+            Addr::unchecked("admin1"),
+            vote_admin_3.clone(),
+            &VoteExecuteMsg::Accept {},
+            &[],
+        )
+        .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(admin.clone(), &AdminQueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp.admins.len(), 4);
     }
 }
